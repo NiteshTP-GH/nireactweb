843db1f20b49f22f580fb043740dd3fa697f68ea
JGISC59 <jgisc59@JGISC.edu>
2022-02-15 10:27:50 +0530

    first commit

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,16 @@
+
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faPhone, faLocation, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 const Footer = () => {
     return (
         <>
             <section className="bg-info py-5 text-white">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-4">1</div>
-                        <div className="col-md-4">2</div>
-                        <div className="col-md-4">3</div>
+                        <div className="col-md-4"><FontAwesomeIcon icon={faPhone} /></div>
+                        <div className="col-md-4"><FontAwesomeIcon icon={faLocation} /></div>
+                        <div className="col-md-4"><FontAwesomeIcon icon={faEnvelope} /></div>
                     </div>
                 </div>                
             </section>
