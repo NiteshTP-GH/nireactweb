c3eebb9879e498530238880d5149f9f92644fc55
JGISC59 <jgisc59@JGISC.edu>
2022-03-07 10:19:11 +0530

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -58,7 +58,7 @@ const Home = () => {
                     <Card>
                         <Card.Img variant="top" src={cardimg} />
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
+                            <Card.Title>Lorem Ipsum is simply</Card.Title>
                             <Card.Text>
                             Some quick example text to build on the card title and make up the bulk of
                             the card's content.
